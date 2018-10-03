7849c48450f5a22fed01ba7239a379d045877a7d
Tiago Aparício <tiago.aparicio@innowave.tech>
2018-10-03 10:22:28 +0100

    Step5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { environment } from '@env/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tasteful-angular';
+  constructor() {
+    console.log({ environment });
+  }
 }
