ca9f00e3b34ea0f31ea3c5d59004cf6765b46dd7
Tiago Aparicio <tiago89mma@gmail.com>
2018-10-03 14:57:17 +0100

    todo-app/step4

--- src/app/todo-app/todo-app-routing.module.ts
+++ src/app/todo-app/todo-app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TodoAppComponent } from './todo-app.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 const routes: Routes = [{
   path: '',
   component: TodoAppComponent,
+  children: [                                                             // add
+    { path: ':index', component: TodoItemComponent },                   // add
+  ]                                                                       // add
 }];
 
 @NgModule({
--- src/app/todo-app/todo-app.module.ts
+++ src/app/todo-app/todo-app.module.ts
@@ -3,15 +3,15 @@ import { CommonModule } from '@angular/common';
 import { FormsModule }   from '@angular/forms';
 import { TodoAppRoutingModule } from './todo-app-routing.module';
 import { TodoAppComponent } from './todo-app.component';
-import { TodoAdderComponent } from './todo-adder/todo-adder.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
-
+import { TodoAdderComponent } from './shared/todo-adder/todo-adder.component';
+import { TodoListComponent } from './shared/todo-list/todo-list.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 @NgModule({
   imports: [
     CommonModule,
     TodoAppRoutingModule,
     FormsModule
   ],
-  declarations: [TodoAppComponent, TodoAdderComponent, TodoListComponent]
+  declarations: [TodoAppComponent, TodoAdderComponent, TodoListComponent, TodoItemComponent]
 })
 export class TodoAppModule { }
