c0a596b378f0df05a1964651cb8cfac6d0f3b0a1
Tiago Aparicio <tiago89mma@gmail.com>
2018-10-03 12:06:40 +0100

    todo-appStep1

--- src/app/todo-app/todo-app.module.ts
+++ src/app/todo-app/todo-app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule }   from '@angular/forms';
 import { TodoAppRoutingModule } from './todo-app-routing.module';
 import { TodoAppComponent } from './todo-app.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    TodoAppRoutingModule
+    TodoAppRoutingModule,
+    FormsModule
   ],
   declarations: [TodoAppComponent]
 })
