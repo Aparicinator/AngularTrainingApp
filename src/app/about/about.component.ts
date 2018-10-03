e1e5a976d2da9385352a34a3d27b5d36cfe4ed6e
Tiago Aparicio <tiago89mma@gmail.com>
2018-10-03 11:46:29 +0100

    Step7

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Increment, Decrement } from '@state/counter/counter.actions';
+import { State } from '@state/counter/counter.state';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  count$: Observable<number>;
 
-  constructor() { }
+  constructor(private store: Store<State>) {
+    this.count$ = store.pipe(select('count'));
+  }
+
+  increment() {
+    this.store.dispatch(new Increment());
+  }
+
+  decrement() {
+    this.store.dispatch(new Decrement());
+  }
 
   ngOnInit() {
   }
